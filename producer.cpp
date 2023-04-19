ab808cf82e66a95adcc5c7e45502b6da575c7d3e
shane-wechsler <127889814+shane-wechsler@users.noreply.github.com>
2023-04-18 17:55:04 -0700

    new prod, simplified sema btc

--- producer.cpp
+++ producer.cpp
@@ -17,74 +17,44 @@
 void* producer(void* ptr){
 
     PRODUCER* data = (PRODUCER *)ptr;
-    while (data->broker->numRequests < data->broker->maxRequests) {
-        if(data->requestType==Bitcoin && data->broker->inRequestQueue[data->requestType] >= MAX_BTC_REQUESTS){
-            sem_wait(&data->broker->btcEmpty);
-            // Here, we want to start a wait for btc requests only
-            sem_wait(&data->broker->empty); // wait until there is space in the buffer
-            usleep(data->requestTime*1000);
-            pthread_mutex_lock(&data->broker->bufferMutex); // acquire buffer mutex before adding item to buffer
-            if (data->broker->boundedBuffer.size() <= BUFFER_SIZE &&
-                data->broker->numRequests < data->broker->maxRequests &&
-                data->broker->inRequestQueue[Bitcoin] < MAX_BTC_REQUESTS) {
-                pthread_mutex_unlock(&data->broker->bufferMutex); // acquire buffer mutex before adding item to buffer
 
-                pthread_mutex_lock(&data->broker->bufferMutex); // acquire buffer mutex before adding item to buffer
-                data->broker->boundedBuffer.push(Bitcoin);
-                pthread_mutex_unlock(&data->broker->bufferMutex);
-                data->broker->numRequests++;
-                data->broker->produced[Bitcoin]++;
-                data->broker->inRequestQueue[Bitcoin]++;
+    while (true) {
 
-                if(data->broker->numRequests <= data->broker->maxRequests){
-                    log_request_added(Bitcoin, data->broker->produced, data->broker->inRequestQueue);
-                }
-            }else{
-                pthread_mutex_unlock(&data->broker->bufferMutex); // acquire buffer mutex before adding item to buffer
-            }
-            //pthread_mutex_unlock(&data->broker->bufferMutex);
-            //sem_post(&data->broker->maxReq);
-            sem_post(&data->broker->full);
+        /*
+         * I think we want to focus on using the usleep() function to loop between BTC
+         * and ETH pushes to the buffer, whilst checking if there
+         * is available space on the buffer (respectively). We will
+         * do BTC pushes first and then and ETH push first. In the
+         * case that there is no delay for either, we must ensure
+         * that it does not get stuck only publishing BTC pushes.
+         */
+        if(data->requestType == Bitcoin){
             sem_wait(&data->broker->btcFull);
         }
-
         sem_wait(&data->broker->empty); // wait until there is space in the buffer
         usleep(data->requestTime*1000);
         pthread_mutex_lock(&data->broker->bufferMutex); // acquire buffer mutex before adding item to buffer
-        /* BTC or ETH Request Push */
+
+        //std::cout << "Test - Current Number of BTC requests right now: " << broker->inRequestQueue[Bitcoin] << std::endl;
+        //std::cout << "Test - Current Number of ETH requests right now: " << broker->inRequestQueue[Ethereum] << std::endl;
+
+        /* BTC Request Push */
         if (data->broker->boundedBuffer.size() <= BUFFER_SIZE &&
-            data->broker->numRequests < data->broker->maxRequests &&
-            data->broker->inRequestQueue[Bitcoin] < MAX_BTC_REQUESTS) {
-            pthread_mutex_unlock(&data->broker->bufferMutex);
-            //if(data->requestType == Bitcoin && data->broker->inRequestQueue[data->requestType] >= MAX_BTC_REQUESTS){
-            //    while(data->broker->inRequestQueue[data->requestType] >= MAX_BTC_REQUESTS);
-            //}
-            pthread_mutex_lock(&data->broker->bufferMutex); // acquire buffer mutex before adding item to buffer
+            data->broker->numRequests < data->broker->maxRequests) {
             data->broker->boundedBuffer.push(data->requestType);
             pthread_mutex_unlock(&data->broker->bufferMutex);
             /*Increase number of total BTC requests, and increase number of BTC requests in queue*/
             data->broker->numRequests++;
-            //pthread_mutex_lock(&data->broker->producerMutex);
             data->broker->produced[data->requestType]++;
-            //pthread_mutex_unlock(&data->broker->producerMutex);
             data->broker->inRequestQueue[data->requestType]++;
-            //pthread_mutex_lock(&data->broker->producerMutex);
-            if(data->broker->numRequests  <= data->broker->maxRequests){
-                log_request_added(data->requestType, data->broker->produced, data->broker->inRequestQueue);
-            }
-            if(data->requestType == Bitcoin){
-                sem_post(&data->broker->btcFull); // signal that btc limit is nearing capacity
-            }
+            log_request_added(Bitcoin, data->broker->produced, data->broker->inRequestQueue);
+                /* TODO: Do we sleep() here, or before the above counter variable calls? Pretty sure down here is correct. */
         }
-        else{
-            pthread_mutex_lock(&data->broker->bufferMutex);
-        }
-        //pthread_mutex_unlock(&data->broker->bufferMutex);
-        //sem_post(&data->broker->maxReq);
+        pthread_mutex_unlock(&data->broker->bufferMutex);
+
+        /*TODO: Figure out if we need mutex locking/unlocking BETWEEN BTC/ETH pushes to buffer.
+         * Keeping 'em here for now.*/
+
         sem_post(&data->broker->full); // signal that a new item has been added to the buffer
-        if(data->broker->numRequests >= data->broker->maxRequests){
-            break;
-        }
     }
-    return NULL;
 }
